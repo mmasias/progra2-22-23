8309b9259e7fd89f3a72921dc6f22fbb2953f67f
ikercelaya <114479408+ikercelaya@users.noreply.github.com>
2023-02-25 17:50:37 +0100

    Add files via upload

---
+++ ejercicios/entregas/ikerCelaya/EX000PooIntroduction/BankAccountTest.java
@@ -0,0 +1,27 @@
+Package Poo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Test;
+
+public class BankAccountTest {
+
+    @Test
+    void getOwner() {
+
+    }
+
+    @Test
+    void setOwner() {
+
+    }
+
+    @Test
+    void getAmount() {
+
+    }
+
+    @Test
+    void setAmount() {
+
+    }
+}
