0b37be64b3e6fad6d11c24f0ceebaaac84b6653b
andriuw08 <andriuw.yepez@gmail.com>
2023-02-26 22:47:39 +0100

    docs(add project): ex000 finalized

--- ejercicios/entregas/andriuwYepez/EX000-PooIntroduccion/src/Main.java
+++ ejercicios/entregas/andriuwYepez/EX000-PooIntroduccion/src/Main.java
@@ -1,5 +1,17 @@
+import Poo.BankAccount;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.setAccountHolder("John");
+        System.out.println(bankAccount.getAccountHolder());
+        bankAccount.setAmount(200.50);
+        System.out.println(bankAccount.getAmount());
+        bankAccount.setAmountWithdraw(100.50);
+        System.out.println(bankAccount.getAmountWhithdraw());
+        bankAccount.setDeposit(50.50);
+        System.out.println(bankAccount.getDeposit());
+
     }
 }
