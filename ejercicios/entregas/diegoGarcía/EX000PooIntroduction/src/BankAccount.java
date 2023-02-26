f6fc5e0a828326876d09f73897e4aecd4864acfd
Dievexx <diego30sscc@gmail.com>
2023-02-26 16:21:41 +0100

    feature(method): add getter and setter

--- ejercicios/entregas/diegoGarcía/EX000PooIntroduction/src/BankAccount.java
+++ ejercicios/entregas/diegoGarcía/EX000PooIntroduction/src/BankAccount.java
@@ -1,2 +1,27 @@
 public class BankAccount {
+    private String holder;
+    private double amount;
+
+    public BankAccount(String holder, double amount) {
+        this.holder = holder;
+        this.amount = amount;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    
 }
