fdf59947013b7b72c319b1b99f712616881e6da4
Pablo <pablo.arce1@alumnos.uneatlantico.es>
2023-03-03 10:39:50 +0100

    exercise 1 correction

--- ejercicios/entregas/PabloArceDeAldecoa/EX000PooIntroduction/src/Poo/BankAccountTestOld.java
+++ ejercicios/entregas/PabloArceDeAldecoa/EX000PooIntroduction/src/Poo/BankAccountTestOld.java
@@ -1,43 +1,28 @@
-package Poo;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class BankAccountTest {
-
+/*package Poo;
+class BankAccountTestOld {
     @Test
-    void test1(){
-        BankAccount bankAccount = new BankAccount();
-        bankAccount.setTitular("Pablo");
-        bankAccount.setAmount(200);
-    }
 
-    @Test
-    void test2() {
+    void test2(){
         BankAccount bankAccount = new BankAccount();
         bankAccount.deposit(300);
     }
-    @Test
     void test3(){
         BankAccount bankAccount = new BankAccount();
         bankAccount.deposit(300);
         bankAccount.withdraw(150);
     }
-    @Test
     void test4(){
         BankAccount bankAccount = new BankAccount();
         bankAccount.deposit(-100);
     }
-    @Test
     void test5(){
         BankAccount bankAccount = new BankAccount();
         bankAccount.withdraw(-100);
     }
-    @Test
     void test6(){
         BankAccount bankAccount = new BankAccount();
         bankAccount.deposit(-1000);
-        System.out.println(bankAccount.getAmount());
     }
 }
+
+ */
