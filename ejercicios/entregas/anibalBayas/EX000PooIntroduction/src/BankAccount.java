88ec2c44ac726a77e6bbef52219ce27b34fa6b39
anibalbg <anibal.bayas@alumnos.uneatlantico.es>
2023-03-03 13:47:18 +0100

    New Files

--- ejercicios/entregas/anibalBayas/EX000PooIntroduction/src/BankAccount.java
+++ ejercicios/entregas/anibalBayas/EX000PooIntroduction/src/BankAccount.java
@@ -1,2 +1,39 @@
 public class BankAccount {
+        private String owner;
+        private double amount;
+
+        public BankAccount(String owner, double amount) {
+            this.owner = owner;
+            this.amount = amount;
+        }
+
+        public String getOwner() {
+            return this.owner;
+        }
+
+        public void setOwner(String owner) {
+            this.owner = owner;
+        }
+
+        public double getAmount() {
+            return amount;
+        }
+
+        public void setAmount(double amount) {
+            this.amount = amount;
+        }
+
+        public void deposit(double amount) {
+            if (amount > 0) {
+                this.amount = this.amount + amount;
+            }
+        }
+
+        public void withdraw(double amount) {
+            this.amount = this.amount - amount;
+            if (this.amount < 0) {
+                this.amount = 0;
+            }
+        }
+
 }
