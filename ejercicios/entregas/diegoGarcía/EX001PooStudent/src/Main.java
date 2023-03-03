e3e948df8d43ec3456dbeec12e35cecd13e573d4
Dievexx <diego30sscc@gmail.com>
2023-03-03 11:46:30 +0100

    feature(classes added): added the classes of Student and Subject also the test class of Subject

---
+++ ejercicios/entregas/diegoGarcía/EX001PooStudent/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Subject subject = new Subject("Math", 5);
+
+
+        Student firstStudent = new Student("John", "Doe", "123456", 5.0f);
+        Student secondStudent = new Student("Jane", "Doe", "123457", 4.0f);
+        Student thirdStudent = new Student("John", "Smith", "123458", 3.0f);
+
+        subject.addStudent(firstStudent);
+        subject.addStudent(secondStudent);
+        subject.addStudent(thirdStudent);
+
+
+        System.out.println("The average grade is: " + subject.getAverageGrade());
+        System.out.println("The best student is: " + subject.getBestStudent());
+        System.out.println("The worst student is: " + subject.getWorstStudent());
+
+    }
+
+
+}
