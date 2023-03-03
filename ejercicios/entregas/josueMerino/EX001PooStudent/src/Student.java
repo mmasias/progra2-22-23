52f7a53093090cb6cc4bcd1cffe2d4ca7b3e6121
josueMerino <josue.merino@alumnos.uneatlantico.es>
2023-03-03 10:55:59 +0100

    feature: EX001PooStudent project created

---
+++ ejercicios/entregas/josueMerino/EX001PooStudent/src/Student.java
@@ -0,0 +1,41 @@
+public class Student {
+    private String name;
+    private String surname;
+    private String id;
+    private float grade;
+
+    public Student(String name, String surname, String id, float grade) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.grade = grade;
+    }
+
+    public void setGrade(float grade) {
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
