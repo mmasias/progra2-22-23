74e81e769eade3864b84bdc625abd7cb0f3721d4
Dievexx <diego30sscc@gmail.com>
2023-03-03 17:06:50 +0100

    feature(methods): added the get average test

--- ejercicios/entregas/diegoGarcía/EX001PooStudent/src/SubjectTest.java
+++ ejercicios/entregas/diegoGarcía/EX001PooStudent/src/SubjectTest.java
@@ -14,6 +14,7 @@ class SubjectTest {
         int actualLength = subject.getPosition();
 
         assertEquals(expectedLength, actualLength);
+
     }
 
     @Test
@@ -22,5 +23,17 @@ class SubjectTest {
 
     @Test
     void getAverage() {
+        Student testStudent = new Student("Diego", "Garcia", "30", 9.0f);
+        Student testStudent2 = new Student("Pablo", "Garcia", "31", 6.0f);
+        Subject subject = new Subject("POO", 58);
+
+        subject.addStudent(testStudent);
+        subject.addStudent(testStudent2);
+        subject.getAverageGrade();
+
+        float expectedAverage = 7.5f;
+        float actualAverage = subject.getAverageGrade();
+
+        assertEquals(expectedAverage, actualAverage);
     }
 }
