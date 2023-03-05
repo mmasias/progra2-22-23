28366ab8c76f886a996ad5be5605e3ed3079cc79
ikercelaya <iker.celaya@alumnos.uneatlantico.es>
2023-03-05 13:53:51 +0100

    EX001-PooStudent

---
+++ ejercicios/entregas/ikerCelaya/EX001PooStudent/src/StudentTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void getSurname() {
+
+    }
+
+    @Test
+    void getId() {
+
+    }
+
+    @Test
+    void getGrade() {
+
+    }
+
+    @Test
+    void setGrade() {
+
+    }
+
+}
---
+++ ejercicios/entregas/ikerCelaya/EX001PooStudent/src/SubjectTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubjectTest {
+
+    @Test
+    void addStudent() {
+        Student testStudent = new Student("Pedro", "perez", "29", 6.5f);
+        Subject subject = new Subject("Programación II", 58);
+        subject.addStudent(testStudent);
+
+        int expectedLength = 1;
+        int actualLength = subject.getPosition();
+
+        assertEquals(expectedLength, actualLength);
+    }
+
+    @Test
+    void orderStudents() {
+    }
+
+    @Test
+    void getMedian() {
+    }
+
+    @Test
+    void getAverage() {
+    }
+}
