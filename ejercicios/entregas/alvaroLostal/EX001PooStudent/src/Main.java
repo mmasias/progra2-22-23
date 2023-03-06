fe404b99b0095ef80f01849b4c76a6853e25dbfc
lostal <alvaro.lostal@alumnos.uneatlantico.es>
2023-03-06 23:36:20 +0100

    Final version

--- ejercicios/entregas/alvaroLostal/EX001PooStudent/src/Main.java
+++ ejercicios/entregas/alvaroLostal/EX001PooStudent/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         subject.addStudent(thirdStudent);
 
         System.out.println("The best student is: " + subject.getBestStudent());
-        System.out.println("The student in the 2nd position is " + subject.getStudent(1).getName() + " " + subject.getStudent(1).getSurname() );
+        System.out.println("The student in the 2nd position is " + subject.getStudent(1).getName() + " " + subject.getStudent(1).getSurname());
         System.out.println("The average grade is: " + subject.getAverageGrade());
         System.out.println("The median grade is: " + subject.getMedian());
         System.out.println("The worst student is: " + subject.getWorstStudent());
