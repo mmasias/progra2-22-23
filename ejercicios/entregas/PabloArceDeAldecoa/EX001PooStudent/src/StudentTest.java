de016c2aabc222595370b504bda31c98af393040
Pablo <pablo.arce1@alumnos.uneatlantico.es>
2023-03-07 00:15:32 +0100

    EX001PooStudent

---
+++ ejercicios/entregas/PabloArceDeAldecoa/EX001PooStudent/src/StudentTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+    Student student;
+    @Test
+    void getGrade() {
+    }
+    @Test
+    void getName() {
+    }
+    @Test
+    void getSurname() {
+    }
+    @Test
+    void getId() {
+    }
+
+}
