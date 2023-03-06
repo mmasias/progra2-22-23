2e6d42a1c957e298b9d0ce43d11cff0d11cae6d8
Valentina Arias Salazar <114306799+valentinarias@users.noreply.github.com>
2023-03-06 23:47:28 +0100

    StudentTest.java

--- ejercicios/entregas/EX001PooStudent/src/StudentTest.java
+++ ejercicios/entregas/EX001PooStudent/src/StudentTest.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class StudentTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void creationTest(){
+        Student testStudent = new Student("Thomas", "Brown", "123455", 2.0f);
+    }
+    @Test
+    void getName() {
+        Student testStudent = new Student("Thomas", "Brown", "123455", 2.0f);
+
+        String expectedName = "Thomas";
+
+        assertEquals(expectedName, testStudent.getName());
+    }
+
+    @Test
+    void getSurname() {
+        Student testStudent = new Student("Thomas", "Brown", "123455", 2.0f);
+
+        String expectedSurname = "Brown";
+
+        assertEquals(expectedSurname, testStudent.getSurname());
+    }
+    @Test
+    void getId() {
+        Student testStudent = new Student("Thomas", "Brown", "123455", 2.0f);
+
+        String expectedId = "123455";
+
+        assertEquals(expectedId, testStudent.getId());
+
+    }
+
+    @Test
+    void getGrade() {
+        Student testStudent = new Student("Thomas", "Brown", "123455", 2.0f);
+
+        float expectedGrade = 2.0f;
+
+        assertEquals(expectedGrade, testStudent.getGrade());
+    }
+
+    @Test
+    void setGrade() {
+        Student testStudent = new Student("Thomas", "Brown", "123455", 2.0f);
+
+        testStudent.setGrade(2.0f);
+        float expectedGrade = 2.0f;
+
+        assertEquals(expectedGrade, testStudent.getGrade());
+    }
+
 }
