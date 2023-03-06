893a9707689a25adbdd0f972a3fe1ef7216b02f7
AndrextA <andrextalarcon@gmail.com>
2023-03-06 20:39:31 +0100

    feature(EX001PooStudent): Creating methods and test

---
+++ ejercicios/entregas/andrésAlarcón/EX001-PooStudent/src/test/java/org/example/StudentTest.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @org.junit.jupiter.api.Test
+    void setGrade() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSurname() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetGrade() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getGrade() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetSurname() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSurname() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setId() {
+    }
+}
---
+++ ejercicios/entregas/andrésAlarcón/EX001-PooStudent/src/test/java/org/example/SubjectTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubjectTest {
+
+    @org.junit.jupiter.api.Test
+    void addStudent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void orderStudents() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMedian() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAverageGrade() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBestStudent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWorstStudent() {
+    }
+}
