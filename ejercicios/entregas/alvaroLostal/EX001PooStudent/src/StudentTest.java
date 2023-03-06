183a203023d049537af3d63556504d3f8a5283d3
lostal <alvaro.lostal@alumnos.uneatlantico.es>
2023-03-06 23:22:45 +0100

    Some changes

--- ejercicios/entregas/alvaroLostal/EX001PooStudent/src/StudentTest.java
+++ ejercicios/entregas/alvaroLostal/EX001PooStudent/src/StudentTest.java
@@ -1,2 +1,56 @@
-public class StudentTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void creationTest(){
+        Student testStudent = new Student("John", "Doe", "12345", 10.0f);
+    }
+    @Test
+    void getName() {
+        Student testStudent = new Student("John", "Doe", "v", 10.0f);
+
+        String expectedName = "John";
+
+        assertEquals(expectedName, testStudent.getName());
+    }
+
+    @Test
+    void getSurname() {
+        Student testStudent = new Student("John", "Doe", "12345", 10.0f);
+
+        String expectedSurname = "Doe";
+
+        assertEquals(expectedSurname, testStudent.getSurname());
+    }
+    @Test
+    void getId() {
+        Student testStudent = new Student("John", "Doe", "12345", 10.0f);
+
+        String expectedId = "12345";
+
+        assertEquals(expectedId, testStudent.getId());
+
+    }
+
+    @Test
+    void getGrade() {
+        Student testStudent = new Student("John", "Doe", "12345", 10.0f);
+
+        float expectedGrade = 10.0f;
+
+        assertEquals(expectedGrade, testStudent.getGrade());
+    }
+
+    @Test
+    void setGrade() {
+        Student testStudent = new Student("John", "Doe", "12345", 10.0f);
+
+        testStudent.setGrade(10.0f);
+        float expectedGrade = 10.0f;
+
+        assertEquals(expectedGrade, testStudent.getGrade());
+    }
 }
