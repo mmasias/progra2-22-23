3ca63cbaad9a871c563d9fb82be99ca7b6535371
lostal <alvaro.lostal@alumnos.uneatlantico.es>
2023-03-10 10:56:14 +0100

    I just created the new directories and organised the files

--- ejercicios/entregas/alvaroLostal/EX002UserManagement/src/Main/Main.java
+++ ejercicios/entregas/alvaroLostal/EX002UserManagement/src/Main/Main.java
@@ -1,3 +1,5 @@
+package Main;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
--- ejercicios/entregas/alvaroLostal/EX002UserManagement/src/Main/Option.java
+++ ejercicios/entregas/alvaroLostal/EX002UserManagement/src/Main/Option.java
@@ -1,2 +1,4 @@
+package Main;
+
 public class Option {
 }
