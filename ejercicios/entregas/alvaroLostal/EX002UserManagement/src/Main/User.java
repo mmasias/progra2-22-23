07919cd67280b9d111d876fbd7ad9708cb6dc566
lostal <alvaro.lostal@alumnos.uneatlantico.es>
2023-03-10 11:25:09 +0100

    I just created the .puml file and coded the User class

--- ejercicios/entregas/alvaroLostal/EX002UserManagement/src/Main/User.java
+++ ejercicios/entregas/alvaroLostal/EX002UserManagement/src/Main/User.java
@@ -1,4 +1,42 @@
 package Main;
 
 public class User {
+    private String userName;
+    private String userPassword;
+    public boolean login;
+    public User() {
+        userName = "user";
+        userPassword = "user";
+        login = false;
+    }
+
+    public User(String userName, String userPassword, boolean login) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.login = login;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public boolean isLogin() {
+        return login;
+    }
+
+    public void setLogin(boolean login) {
+        this.login = login;
+    }
 }
