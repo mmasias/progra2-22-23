e6a7972eca8bf01c0683fbfe541a9625de80d5ad
anibalbg <anibal.bayas@alumnos.uneatlantico.es>
2023-03-14 13:31:40 +0100

    Update

---
+++ ejercicios/entregas/anibalBayas/EX002UserManagement/src/main/User.java
@@ -0,0 +1,19 @@
+package main;
+public class User {
+    private String userName;
+    private String userPassword;
+    public boolean login;
+    public User(){
+        userName= "user";
+        userPassword= "user";
+        login= false;
+    }
+
+    public User(String userName, String userPassword, boolean login){
+        this.userName= userName;
+        this.userPassword= userPassword;
+        this.login=login;
+        }
+    public String Name;
+    private String Passsword;
+}
---
+++ ejercicios/entregas/anibalBayas/Geometry/src/Main/ColoredCricle.java
@@ -0,0 +1,9 @@
+package Main;
+
+public class ColoredCricle {
+    private int color;
+    public ColoredCricle(int color){
+        this.color = color;
+    }
+
+}
