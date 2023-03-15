36d28c7c9205a1e0cf0f545036ebaa16b2da1e71
josueMerino <josue.merino@alumnos.uneatlantico.es>
2023-03-16 00:16:46 +0100

    test: ManagerTest created

---
+++ ejercicios/entregas/josueMerino/EX002UserManagement/src/test/ManagerTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void login() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void register() {
+    }
+}
