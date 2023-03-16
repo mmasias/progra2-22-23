537535c35db16ef51fe4d0533218577a51cc0970
Aurelio Ochoa <61554216+aurelioochoa@users.noreply.github.com>
2023-03-16 23:53:24 +0100

    EX002UserManagement

---
+++ ejercicios/entregas/EX002UserManagement/src/User.java
@@ -0,0 +1,35 @@
+class User {
+    private String login;
+    private String password;
+    private String name;
+
+    public User(String login, String password, String name) {
+        this.login = login;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
