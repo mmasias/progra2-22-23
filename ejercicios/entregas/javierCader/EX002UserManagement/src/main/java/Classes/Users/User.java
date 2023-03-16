f046861b7abe38847e3941f3f67681793a8b6e4f
JavierCader <113170731+JavierCader@users.noreply.github.com>
2023-03-16 16:54:28 +0100

    First Correction Rounds

--- ejercicios/entregas/javierCader/EX002UserManagement/src/main/java/Classes/Users/User.java
+++ ejercicios/entregas/javierCader/EX002UserManagement/src/main/java/Classes/Users/User.java
@@ -1,14 +1,18 @@
 package Classes.Users;
 
+import Utils.Interfaces.MenuItem;
+
 public abstract class User {
     protected String username;
     protected String password;
     protected boolean login;
+    protected MenuItem[] options;
 
-    public User(String username, String password, boolean login) {
+    public User(String username, String password, boolean login, MenuItem[] options) {
         this.username = username;
         this.password = password;
         this.login = login;
+        this.options = options;
     }
 
     public void setUsername(String username) {
