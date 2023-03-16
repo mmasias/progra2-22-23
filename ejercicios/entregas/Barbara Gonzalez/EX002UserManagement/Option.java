eda06873cde3948c455b86ea97813525bafd2e3a
Barbara Gonzalez <barbara.gonzalez@alumnos.uneatlantico.es>
2023-03-16 21:36:13 +0100

    EXOO2UserManagement

---
+++ ejercicios/entregas/Barbara Gonzalez/EX002UserManagement/Option.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Option {
+
+    private List<String> options;
+    private int maxOptions;
+    private int selectedOption;
+
+    public Option(int maxOptions) {
+        this.options = new ArrayList<String>();
+        this.maxOptions = maxOptions;
+        this.selectedOption = 0;
+    }
+
+    public void addOption(String option) {
+        if (options.size() < maxOptions) {
+            options.add(option);
+        }
+    }
+
+    public String getOption(int index) {
+        if (index >= 0 && index < options.size()) {
+            return options.get(index);
+        }
+        return null;
+    }
+
+    public int getSelectedOption() {
+        return selectedOption;
+    }
+
+    public void setSelectedOption(int selectedOption) {
+        if (selectedOption >= 0 && selectedOption < options.size()) {
+            this.selectedOption = selectedOption;
+        }
+    }
+
+    public int getMaxOptions() {
+        return maxOptions;
+    }
+
+    public void setMaxOptions(int maxOptions) {
+        this.maxOptions = maxOptions;
+    }
+
+}
---
+++ ejercicios/entregas/Barbara Gonzalez/TestEX002/ManagerTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ManagerTest {
+    private Manager manager;
+
+    @BeforeEach
+    void setUp() {
+        manager = new Manager(10, 5);
+        manager.addUser(new User("user1", "pass1", "User 1"));
+        manager.addUser(new User("user2", "pass2", "User 2"));
+        manager.addMenuOption("Option 1");
+        manager.addMenuOption("Option 2");
+        manager.addMenuOption("Option 3");
+    }
+
+    @Test
+    void testAuthenticateSuccess() {
+        assertTrue(manager.authenticate("user1", "pass1"));
+        assertNotNull(manager.getAuthenticatedUser());
+        assertEquals("User 1", manager.getAuthenticatedUser().getName());
+    }
+
+    @Test
+    void testAuthenticateFailure() {
+        assertFalse(manager.authenticate("user1", "wrongpassword"));
+        assertNull(manager.getAuthenticatedUser());
+    }
+
+    @Test
+    void testShowMenuSuccess() {
+        manager.authenticate("user1", "pass1");
+        manager.showMenu();
+    }
+
+    @Test
+    void testShowMenuFailure() {
+        manager.showMenu();
+    }
+
+    @Test
+    public void testAuthenticationFailed() {
+        User user1 = new User("user1", "password1", "User One");
+        User user2 = new User("user2", "password2", "User Two");
+        User user3 = new User("user3", "password3", "User Three");
+
+        Manager manager = new Manager(10,5);
+        manager.addUser(user1);
+        manager.addUser(user2);
+        manager.addUser(user3);
+
+        boolean result = manager.authenticate("user4", "password4");
+
+        assertFalse(result);
+        assertNull(manager.getAuthenticatedUser());
+    }
+}
