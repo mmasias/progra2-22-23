d9412f98f813c8ea460e60284808d95904569ced
sara-patino <sara.patino@alumnos.uneatlantico.com>
2023-03-17 11:01:04 +0100

    update files

---
+++ ejercicios/soluciones/EX002UserManagement/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Manager manager = new Manager();
+        User user = new User("user", "password");
+        Option options = new Option();
+        System.out.println("Welcome!");
+        System.out.println("username and password:");
+        System.out.println("Username: ");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+        System.out.println("Password: ");
+        String userPassword = scanner.nextLine();
+        options.setUser(user);
+        if(userName.equals(user.getUserName()) && userPassword.equals(user.getUserPassword())){
+            System.out.println("Login successful");
+            System.out.println("Welcome " + user.getUserName());
+            options.showOptions();
+            options.getSelectedOption();
+        } else {
+            System.out.println("Login failed");
+
+        }
+
+    }
+
+}
--- ejercicios/soluciones/EX002UserManagement/src/Options.java
+++ ejercicios/soluciones/EX002UserManagement/src/Options.java
@@ -1,7 +1,10 @@
 package main;
+import java.util.Scanner;
 
 public class Options {
     private String[] options;
+    private Manager manager;
+    private User user;
 
     public Options(String[] options) {
         this.options = options;
@@ -13,5 +16,52 @@ public class Options {
 
     public void setOptions(String[] options) {
         this.options = options;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public Manager getManager() {
+        return this.manager;
+    }
+    public User getUser() {
+        return this.user;
+    }
+
+    public void showOptions(){
+        System.out.println("1: pizza");
+        System.out.println("2: pasta");
+        System.out.println("3: salad");
+        System.out.println("4: hamburger");
+        System.out.println("5: Exit");
+
+    }
+
+    public void getSelectedOption(){
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        switch (option){
+            case 1:
+                System.out.println("pizza");
+                break;
+            case 2:
+                System.out.println("pasta");
+                break;
+            case 3:
+                System.out.println("salad");
+                break;
+            case 4:
+                System.out.println("hamburger");
+                break;
+            case 5:
+                System.out.println("Exit");
+                break;
+            default:
+                System.out.println("Wrong option, choose correctly");
+                break;
+        }
     }
 }
