e0e545e655fad126128caf5b5d8262a1577440ea
Elder Bol <elder.bol@uneatlantico.es>
2023-03-17 13:25:10 +0100

    fix(typo): New solution

---
+++ ejercicios/soluciones/EX002UserManagementExtended/src/Main.java
@@ -0,0 +1,15 @@
+import main.Manager;
+
+public class Main {
+
+    Manager manager;
+
+    public static void main(String[] args) {
+
+        System.out.println("----------------------------------");
+        System.out.println("- System Login                   - ");
+        System.out.println("----------------------------------");
+
+
+    }
+}
