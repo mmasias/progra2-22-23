16eeab96105aea99be03db669b1fccb3365f492b
sara-patino <sara.patino@alumnos.uneatlantico.com>
2023-03-17 12:02:48 +0100

    added all of the classes

---
+++ ejercicios/entregas/saraPatino/EX003Inheritance/src/Main/User.java
@@ -0,0 +1,46 @@
+package Main;
+
+public class User {
+    private String username;
+    private String password;
+    private String name;
+
+    public User(String username, String password, String name) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAdmin() {
+        if(this.username == username && this.password == password){
+            return true;
+        }  else {
+            return false;
+        }
+    }
+
+}
