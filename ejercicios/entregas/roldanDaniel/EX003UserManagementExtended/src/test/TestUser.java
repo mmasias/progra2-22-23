5bc6c8c0f6cf74480bbf0c5ee2d64625a7d58771
Daniel <daniel.roldan@alumnos.uneatlantico.es>
2023-03-21 08:00:42 +0100

    test(class): TestUser has been completed
    Signed-off-by: Daniel <daniel.roldan@alumnos.uneatlantico.es>

--- ejercicios/entregas/roldanDaniel/EX003UserManagementExtended/src/test/TestUser.java
+++ ejercicios/entregas/roldanDaniel/EX003UserManagementExtended/src/test/TestUser.java
@@ -1,6 +1,62 @@
 package src.test;
 
-public class TestUser {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import src.main.User;
+
+public class TestUser  {
 
 
+    @Test
+    void getLogin() {
+
+        User userTest = new User();
+        userTest.setLogin("daniel@alumnos.com");
+        Assertions.assertEquals(userTest.getLogin(),"daniel@alumnos.com");
+    }
+
+    @Test
+    void getPassword() {
+
+        User userTest = new User();
+        userTest.setPassword("1234");
+        Assertions.assertEquals(userTest.getPassword(),"1234");
+
+    }
+
+    @Test
+    void getName() {
+
+        User userTest = new User();
+        userTest.setName("Daniel Roldan");
+        Assertions.assertEquals(userTest.getName(),"Daniel Roldan");
+    }
+
+    @Test
+    void setLogin() {
+
+        User userTest = new User();
+        Assertions.assertEquals(userTest.getLogin(),null);
+        userTest.setLogin("daniel@alumnos.com");
+        Assertions.assertEquals(userTest.getLogin(),"daniel@alumnos.com");
+    }
+
+    @Test
+    void setPassword() {
+
+        User userTest = new User();
+        Assertions.assertEquals(userTest.getPassword(),null);
+        userTest.setPassword("321");
+        Assertions.assertEquals(userTest.getPassword(),"321");
+    }
+
+    @Test
+    void setName() {
+
+        User userTest = new User();
+        Assertions.assertEquals(userTest.getName(),null);
+        userTest.setName("Elder Bol");
+        Assertions.assertEquals(userTest.getName(),"Elder Bol");
+
+    }
 }
