efbcceea2c1f397c9336d3ecf1a2b15e224e2a87
Daniel <daniel.roldan@alumnos.uneatlantico.es>
2023-03-22 06:56:12 +0100

    feature(method): all has been added
    Signed-off-by: Daniel <daniel.roldan@alumnos.uneatlantico.es>

--- ejercicios/entregas/roldanDaniel/EX003UserManagementExtended/src/main/Option.java
+++ ejercicios/entregas/roldanDaniel/EX003UserManagementExtended/src/main/Option.java
@@ -1,4 +1,61 @@
 package src.main;
 
 public class Option {
+
+    private String[] options;
+    private String selectedOption;
+    private int maxOptions;
+
+
+    public Option() {
+
+        this.maxOptions = 5;
+
+        String[] options = new String[maxOptions];
+
+
+    }
+
+    public String[] getOptions() {
+
+        return options;
+    }
+
+    public void selectOption(int index) {
+
+
+        if (index < this.options.length && index > 0) {
+            System.out.println("is possible search.... searching....");
+            System.out.println();
+
+            selectedOption = this.options[index - 1];
+
+            System.out.println("The selected option is: " + selectedOption);
+
+        } else if (index < 1) {
+            System.out.println("Admite valores positivos > 0");
+        } else {
+            System.out.println("No hay tantas opciones en el menú, ingresa un valor < introducido");
+        }
+
+
+    }
+
+    public String getSelectedOption() {
+        return selectedOption;
+    }
+
+    public void addOption(String option) {
+
+        if (this.maxOptions < this.options.length) {
+
+            this.options[0] = option;
+
+        } else {
+
+            System.out.println("no capacity");
+
+        }
+
+    }
 }
