6101dedf8ef8ae3b44c809d4bc6a1391af67d368
Daniel <daniel.roldan@alumnos.uneatlantico.es>
2023-03-22 06:28:08 +0100

    fix(constructor): double constructor without input parameters
    Signed-off-by: Daniel <daniel.roldan@alumnos.uneatlantico.es>

--- ejercicios/entregas/roldanDaniel/EX003UserManagementExtended/src/main/User.java
+++ ejercicios/entregas/roldanDaniel/EX003UserManagementExtended/src/main/User.java
@@ -5,7 +5,7 @@ public class User {
     private String login;
     private String password;
     private String name;
-    public User(){
+    public User(String login, String password, String name){
 
         this.login=login;
         this.password=password;
@@ -13,6 +13,9 @@ public class User {
 
     }
 
+    public User(){
+
+    }
     public String getLogin(){
         return this.login;
     }
