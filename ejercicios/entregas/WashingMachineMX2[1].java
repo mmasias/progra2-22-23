ef5fcdb1ddf549aab0d3a71f2526291fb42c8553
RaulPlayo <114569807+RaulPlayo@users.noreply.github.com>
2023-03-22 18:18:46 +0100

    Delete WashingMachineMX2[1].java

--- ejercicios/entregas/WashingMachineMX2[1].java
+++
@@ -1,9 +0,0 @@
-package com.progra.washingmachine;
-
-public class WashingMachineMX2  extends WashingMachineMX1 {
-    public WashingMachineMX2 () {
-        super(); //WashingMachineMX1
-        selector = new TemperatureSelector();
-
-    }
-}
