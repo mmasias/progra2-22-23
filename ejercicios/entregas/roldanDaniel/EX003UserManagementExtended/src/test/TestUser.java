dd6cb0d444792d53c697b96420bbc2deef772cd5
Daniel <daniel.roldan@alumnos.uneatlantico.es>
2023-03-22 06:30:29 +0100

    docs(class): TestUser and AdminUser together in TestUser
    Signed-off-by: Daniel <daniel.roldan@alumnos.uneatlantico.es>

--- ejercicios/entregas/roldanDaniel/EX003UserManagementExtended/src/test/TestUser.java
+++ ejercicios/entregas/roldanDaniel/EX003UserManagementExtended/src/test/TestUser.java
@@ -2,6 +2,7 @@ package src.test;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import src.main.AdminUser;
 import src.main.User;
 
 public class TestUser  {
@@ -58,5 +59,31 @@ public class TestUser  {
         userTest.setName("Elder Bol");
         Assertions.assertEquals(userTest.getName(),"Elder Bol");
 
+    }
+
+    static class AdminUserTest extends TestUser {
+
+        @Test
+        void setAdmin() {
+            AdminUser user = new AdminUser("pedro","","",false);
+            user.setAdmin("Diel","das", "DAniel", true);
+            boolean validate = user.isAdmin();
+            Assertions.assertEquals(validate, true);
+
+        }
+
+
+
+        @Test
+        void isAdmin() {
+
+            AdminUser user = new AdminUser("","","",true);
+
+            boolean validate = user.isAdmin();
+            Assertions.assertEquals(validate, true);
+
+
+
+        }
     }
 }
