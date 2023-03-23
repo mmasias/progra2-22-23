5627c0f168a01458cc216b520131f43edc51764d
Sergio Moreno <113471709+srgiom@users.noreply.github.com>
2023-03-23 23:00:49 +0100

    Delete ejercicios/entregas/sergioMoreno/EX003 directory

--- ejercicios/entregas/sergioMoreno/EX003/MenuOptionTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.jupiter.api.Assertions.*;
-
-class MenuOptionTest {
-
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-        MenuOption menu = new MenuOption("Description");
-    }
-}
--- ejercicios/entregas/sergioMoreno/EX003/RegularUser.java
+++
@@ -1,3 +0,0 @@
-public class RegularUser extends User{
-
-}
