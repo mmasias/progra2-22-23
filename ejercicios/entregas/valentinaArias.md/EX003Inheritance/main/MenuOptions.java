9d228651bc986e947f64a0b3690e1a737cfe5dab
Valentina Arias Salazar <114306799+valentinarias@users.noreply.github.com>
2023-03-23 22:52:17 +0100

    Create MenuOptions.java

---
+++ ejercicios/entregas/valentinaArias.md/EX003Inheritance/main/MenuOptions.java
@@ -0,0 +1,17 @@
+package main;
+
+public class MenuOption extends Options{
+	private String description;
+
+	public MenuOption() {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
