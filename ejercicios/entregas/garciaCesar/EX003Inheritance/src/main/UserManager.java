3dc1e149a62574fea8d458f40f530ac7bdc04278
ceesargaarcia <cesar.garcia@alumnos.uneatlantico.es>
2023-03-24 10:15:20 +0100

    EX003UserMagnemet exercise done

---
+++ ejercicios/entregas/garciaCesar/EX003Inheritance/src/main/UserManager.java
@@ -0,0 +1,12 @@
+package main;
+
+public class UserManager extends Manager {
+	public void registerUser(User user) {
+		// registrar usuario
+	}
+
+	public boolean isUserAdmin(User user) {
+		// determinar si el usuario es admin
+		return false;
+	}
+}
