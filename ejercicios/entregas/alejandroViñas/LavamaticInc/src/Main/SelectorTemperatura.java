94cbf3aba73a5387d492264be1a7fda203fc6bdb
Alejandro Viñas <114479197+AlejandroVinas@users.noreply.github.com>
2023-03-24 10:23:07 +0100

    Add files via upload

---
+++ ejercicios/entregas/alejandroViñas/LavamaticInc/src/Main/SelectorTemperatura.java
@@ -0,0 +1,21 @@
+package Main;
+
+public class SelectorTemperatura {
+    public SelectorTemperatura () { _temperatura = 0; }
+    public void fijarTemperatura (int temp) {
+        if ( (temp >= 0) && (temp <= _maxTemperatura) )
+            _temperatura = temp;
+        else System.err.println("Error: Temperatura incorrecta");
+    }
+    public int temperatura () {
+        return ( _temperatura );
+    }
+    public int tempMaxima () {
+        return ( _maxTemperatura );
+    }
+    public int consumoElectr () {
+        return ( _temperatura * 30 / 100 );
+    }
+    private int _temperatura;
+    private final int _maxTemperatura = 90;
+}
