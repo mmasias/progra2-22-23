bcc6b97b0457070eae0606ec07212f536a6de947
Diegoza04 <barrazadiego05@gmail.com>
2023-04-04 13:40:15 +0200

    Classwork Collections

---
+++ ejercicios/entregas/DiegoBarraza/Colections/src/TestColections.java
@@ -0,0 +1,31 @@
+import java.util.*;
+    public class TestColections{
+        public static void main(String[] args) {
+            Collection<String> listaMarcasCoches = new ArrayList<String>();
+            listaMarcasCoches.add("Audi");
+            listaMarcasCoches.add("Porsche");
+            listaMarcasCoches.add("Aston Martin");
+            listaMarcasCoches.add("Ferrari");
+            listaMarcasCoches.add("Mercedes");
+            listaMarcasCoches.add("Seat");
+
+            System.out.println("Número elementos antes de eliminar: " +
+                    listaMarcasCoches.size() );
+
+            System.out.println (listaMarcasCoches.toString() ) ;
+            listaMarcasCoches.remove ("Seat");
+            listaMarcasCoches.remove ("Mercedes");
+
+            System.out.println("Número elementos después de eliminar Seat y Mercedes:"
+                    + listaMarcasCoches.size() ) ;
+
+            System.out.println(listaMarcasCoches.toString () );
+            Iterator<String> it = listaMarcasCoches.iterator();
+            for(int i=0; it.hasNext();i++){
+                String marca = it.next();
+                if(marca.equals("Audi"))
+                    it.remove();
+            }
+            System.out.println(listaMarcasCoches.toString () );
+}
+        }
