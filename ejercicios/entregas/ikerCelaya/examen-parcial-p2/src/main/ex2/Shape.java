5e9a1832f8de28d6bd3d374741a7dbe367fefce3
Iker Celaya <iker.celaya@alumnos.uneatlantico.es>
2023-04-25 13:36:08 +0200

    commit nuevo

--- ejercicios/entregas/ikerCelaya/examen-parcial-p2/src/main/ex2/Shape.java
+++ ejercicios/entregas/ikerCelaya/examen-parcial-p2/src/main/ex2/Shape.java
@@ -16,4 +16,24 @@ public class Shape {
     public void setColor(String color) {
         this.color = color;
     }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public double getArea(){
+        return 0;
+    }
+    public double getPerimeter(){
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
