9f9fcafacc89f4bc1905202142584b033125947e
Elder Bol <elder.bol@uneatlantico.es>
2023-05-09 13:50:52 +0200

    docs(lecture): New example to be done in class

---
+++ temario/Concurrencia/src/Concurrente.java
@@ -0,0 +1,18 @@
+public class Concurrente extends Thread {
+
+    public Concurrente(String name){
+        super(name);
+    }
+    public void run(){
+        for (int i = 0; i <10; i++){
+            System.out.println(i + " - " + this.getName());
+            try {
+                sleep( (long) (Math.random() * 1000) );
+            }catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println("He terminado: " + this.getName());
+    }
+}
