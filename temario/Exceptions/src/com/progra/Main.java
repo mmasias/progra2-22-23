2f6277dd2a12bd8d5b984126b17b00fa34289127
josueMerino <josue.merino@alumnos.uneatlantico.es>
2023-05-10 00:25:11 +0200

    feature(): Reading the files

--- temario/Exceptions/src/com/progra/Main.java
+++ temario/Exceptions/src/com/progra/Main.java
@@ -4,11 +4,15 @@ import com.progra.inventory.Inventory;
 import com.progra.inventory.exceptions.FullStorageException;
 import com.progra.inventory.exceptions.ItemNotFoundException;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FullStorageException, ItemNotFoundException {
+    public static void main(String[] args) throws FullStorageException, ItemNotFoundException, IOException {
 
         Scanner input = new Scanner(System.in);
         String newObject = "";
@@ -22,6 +26,7 @@ public class Main {
             System.out.println("1. Crear un nuevo item en tu inventario manualmente");
             System.out.println("2. Eliminar un item de tu inventario");
             System.out.println("3. Añadir items a tu inventario a partir de un archivo");
+            System.out.println("4. Mostrar los items del inventario");
             System.out.println("0. Salir del programa");
             option = Integer.parseInt(input.nextLine());
 
@@ -46,6 +51,31 @@ public class Main {
                     catch (ItemNotFoundException e) {
                         throw new ItemNotFoundException(e.getMessage());
                     }
+                    break;
+                case 3:
+                    try {
+                        System.out.println("Ingresa la ruta del archivo") ;
+                        String path = input.nextLine();
+
+                        BufferedReader fileToRead = new BufferedReader(new FileReader(path));
+                        String line;
+                        while (fileToRead.readLine() != null)
+                        {
+                            line = fileToRead.readLine();
+                            inv.setItem(line);
+                        }
+
+                    }catch (FileNotFoundException e){
+                        throw new FileNotFoundException(e.getMessage());
+                    } catch (IOException e) {
+                        throw new IOException(e.getMessage());
+                    }
+                    break;
+                case 4:
+                    for (String item: inv.getItems()) {
+                        System.out.println(item);
+                    }
+
             }
 
         }
